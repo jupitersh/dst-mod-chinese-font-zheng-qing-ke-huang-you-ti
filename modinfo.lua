ebd9bc13cb6415be14b415a43b6bc6d688402eee
jupitersh <arxhd.love@qq.com>
2019-12-27 16:39:41 +0800

    first commit

---
+++ modinfo.lua
@@ -0,0 +1,15 @@
+name = "郑庆科黄油体"
+description = [[
+]]
+author = "辣椒小皇纸"
+version = "1.0.0"
+forumthread = ""
+api_version = 10
+icon_atlas = "modicon.xml"
+icon = "modicon.tex"
+
+dst_compatible = true
+client_only_mod = true
+all_clients_require_mod = false
+
+priority = -2019
